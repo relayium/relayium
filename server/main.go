88213119343e299cd34f6d63b8884c7b5256988a
relayium <github@relayium.com>
2026-06-28 08:13:49 +0400

    chore: scaffold Go signaling server and Svelte web client

---
+++ server/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+)
+
+func main() {
+	addr := flag.String("addr", ":8080", "listen address")
+	flag.Parse()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+	log.Printf("relayium signaling server listening on %s", *addr)
+	log.Fatal(http.ListenAndServe(*addr, mux))
+}
